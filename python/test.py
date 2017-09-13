3805037916fda4f5f02bffab48456f6436c72de9
Thony Price <thonyprice@gmail.com>
2017-09-13 17:41:32 +0200

    T/F on leaves

--- python/test.py
+++ python/test.py
@@ -1,5 +1,6 @@
 import monkdata as m
 import dtree
+import drawtree_qt5 as draw
 
 monks = [m.monk1, m.monk2, m.monk3]
 
@@ -17,12 +18,12 @@ def monkEntropyAndInfoGain(monks):
         print("\n")
     return
 
-def getSubsets(n):
-    MONK = m.monk1 # Hard coded due to assignment specification
+def getSubsets(set, n):
     values = m.attributes[n].values
     subsets = []
     for val in values:
         subsets.append(dtree.select(m.monk1, m.attributes[4], val))
+        # print(dtree.select(m.monk1, m.attributes[4], val))
     return subsets
 
 def infoGainOnSubsets(subsets):
@@ -41,15 +42,38 @@ def infoGainOnSubsets(subsets):
         j = 0
         while j < len(m.attributes):
             result[j] += (l[i][j])
-            print(l[i][j])
             j+=1
         i+=1
     """
     Summing over the information gains given by the attributes in different
     nodes on level two shows that attribute 1 gives the best information gain
     """
-    # print(result)
+    print(result)
     return
 
-subsets = getSubsets(4)
-infoGainOnSubsets(subsets)
+def getLeaves(dataSet, a1, a2):
+    a1_domain = m.attributes[a1].values
+    a2_domain = m.attributes[a2].values
+
+    for k in a1_domain:
+        x = dtree.select(dataSet, m.attributes[a1], k)
+        for l in a2_domain:
+            y = dtree.select(x, m.attributes[a2], l)
+            z = dtree.mostCommon(y)
+            print("For " + str(k) + ":" + str(l) + ", " + "most common = " + str(z))
+
+getLeaves(m.monk1, 4, 0)
+draw.drawTree(dtree.buildTree(m.monk1, m.attributes, 2))
+
+#subsets = getSubsets(m.monk1, 4)
+#infoGainOnSubsets(subsets)
+'''
+for att in att5
+
+    for att in att1
+
+x = dtree.select(m.monk1, m.attributes[4], 1)
+y = dtree.select(x, m.attributes[4], 3)
+z = dtree.mostCommon(y)
+print(z)
+'''
