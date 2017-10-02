ed4c8aa6792fd9cab6708bde5e111b3f153d119e
wska <william.skagerstrom@gmail.com>
2017-10-02 15:15:33 +0200

    Added matrix and vector creators.

--- main.py
+++ main.py
@@ -28,5 +28,73 @@ def linearKernel(vectorX, vectorY):
     return (scalar+1)
 
 
-scalarTest = linearKernel([1,2,3],[1,2,3])
-print(scalarTest)
+def pMatrixCreator(dataSet):
+    n = len(dataSet)
+    pMatrix = [[0 for x in range(n)] for y in range(n)]
+
+
+    for i in range(n):
+        x = dataSet[i]
+        for j in range(n):
+            y = dataSet[j]
+            pMatrix[i][j] = x[2]*y[2]*linearKernel(x[:2] ,y[:2])
+    return pMatrix
+
+
+def qhVectorCreator(n):
+    q = [-1]*n
+    h = [0]*n
+    return q,h
+
+
+def gMatrixCreator(n):
+    gMatrix = [[0 for x in range(n)] for y in range(n)]
+    for i in range(n):
+        gMatrix[i][i] = -1
+    return gMatrix
+
+
+
+
+#scalarTest = linearKernel([1,2,3],[1,2,3])
+#print(scalarTest)
+
+'''
+classA=[(random.normalvariate(-1.5 ,1),
+random.normalvariate(0.5 ,1),
+1.0)
+for i in range(5)]+\
+[(random.normalvariate(1.5 ,1),
+random.normalvariate(0.5 ,1),
+1.0)
+for i in range(5)]
+
+
+classB=[(random.normalvariate(0.0 ,0.5),
+random.normalvariate(-0.5 ,0.5),
+-1.0)
+for i in range(10)]
+
+data = classA + classB
+data = random.shuffle(data)
+
+
+
+pylab.hold(True)
+pylab.plot([p[0] for p in classA],
+            [p[1] for p in classA],'bo')
+
+pylab.plot([p[0] for p in classB],
+            [p[1] for p in classB], 'ro')
+
+pylab.show()
+
+'''
+
+#data = [(1,-1,1),(1,0,-1)]
+#matrix = pMatrixCreator(data)
+#print(matrix)
+
+
+#print(classA)
+#print(len(classA))
