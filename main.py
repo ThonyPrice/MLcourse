17f9676d6722b760d8cae00b824248b459d2a68d
wska <william.skagerstrom@gmail.com>
2017-10-02 14:30:09 +0200

    Partial commit. Working linear kernal function.

--- main.py
+++ main.py
@@ -2,3 +2,31 @@ from cvxopt.solvers import qp
 from cvxopt.base import matrix
 
 import numpy, pylab, random, math
+
+
+#r = qp(matrix(P), matrix(q), matrix(G), matrix(h))
+#alpha = list(r['x'])
+
+
+
+
+
+#classA = [(random.normalvariate(-1.5,1), random.normalvariate(0.5,1), 1.0)]
+
+def linearKernel(vectorX, vectorY):
+
+    if(len(vectorX)!=len(vectorY)):
+        print("Vector length not equal.")
+        return 0
+
+
+    scalar = 0
+
+    for i in range(0,len(vectorX)):
+        scalar += vectorX[i]*vectorY[i]
+
+    return (scalar+1)
+
+
+scalarTest = linearKernel([1,2,3],[1,2,3])
+print(scalarTest)
